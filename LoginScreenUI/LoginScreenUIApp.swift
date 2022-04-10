c2eb20133fcdc0ac0dcac55f95f3da37018eca22
Irem Gunay <iremguunay@gmail.com>
2022-04-10 23:37:07 +0300

    Initial Commit

---
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by İrem Günay on 10.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
